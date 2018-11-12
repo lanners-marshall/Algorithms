29bb380912dfce07b197d664b1e0a49959979b7c
marshall <marshall@marshalls-MacBook-Air.local>
2018-11-11 20:51:28 -0800

    got making change working

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,7 +3,20 @@
 import sys
 
 def making_change(amount, denominations):
-  pass 
+  t = [[0 for x in range(5)] for x in range(amount + 1)]
+
+  for i in range(5):
+  	t[0][i] = 1
+
+  for i in range(1, amount + 1):
+  	for j in range(5):
+  		x = t[i - denominations[j]][j] if i - denominations[j] >= 0 else 0
+
+  		y = t[i][j-1] if j >= 1 else 0
+
+  		t[i][j] = x + y
+
+  return t[amount][4]
 
 
 if __name__ == "__main__":
